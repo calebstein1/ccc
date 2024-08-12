69dccee73412bb50d156f90b40c7a238f259823b
Caleb Stein <caleb@calebstein.net>
2024-08-12 00:19:10 -0700

    add string literals for assembler

--- ccc/include/opcodes.h
+++ ccc/include/opcodes.h
@@ -1,63 +1,63 @@
 #ifndef CCC_OPCODES_H
 #define CCC_OPCODES_H
 
-#define OPCODES_LIST   \
-    X(ADC, adc_f)      \
-    X(AND, and_f)      \
-    X(ASL, asl_f)      \
-    X(BCC, bcc_f)      \
-    X(BCS, bcs_f)      \
-    X(BEQ, beq_f)      \
-    X(BIT, bit_f)      \
-    X(BMI, bmi_f)      \
-    X(BNE, bne_f)      \
-    X(BPL, bpl_f)      \
-    X(BRK, brk_f)      \
-    X(BVC, bvc_f)      \
-    X(BVS, bvs_f)      \
-    X(CLC, clc_f)      \
-    X(CLD, cld_f)      \
-    X(CLI, cli_f)      \
-    X(CLV, clv_f)      \
-    X(CMP, cmp_f)      \
-    X(CPX, cpx_f)      \
-    X(CPY, cpy_f)      \
-    X(DEC, dec_f)      \
-    X(DEX, dex_f)      \
-    X(DEY, dey_f)      \
-    X(FOR, for_f)      \
-    X(INC, inc_f)      \
-    X(INX, inx_f)      \
-    X(INY, iny_f)      \
-    X(JMP, jmp_f)      \
-    X(JSR, jsr_f)      \
-    X(LDA, lda_f)      \
-    X(LDX, ldx_f)      \
-    X(LDY, ldy_f)      \
-    X(LSR, lsr_f)      \
-    X(NOP, nop_f)      \
-    X(ORA, ora_f)      \
-    X(PHA, pha_f)      \
-    X(PHP, php_f)      \
-    X(PLA, pla_f)      \
-    X(PLP, plp_f)      \
-    X(ROL, rol_f)      \
-    X(ROR, ror_f)      \
-    X(RTI, rti_f)      \
-    X(RTS, rts_f)      \
-    X(SBC, sbc_f)      \
-    X(SEC, sec_f)      \
-    X(SED, sed_f)      \
-    X(SEI, sei_f)      \
-    X(STA, sta_f)      \
-    X(STX, stx_f)      \
-    X(STY, sty_f)      \
-    X(TAX, tax_f)      \
-    X(TAY, tay_f)      \
-    X(TSX, tsx_f)      \
-    X(TXA, txa_f)      \
-    X(TXS, txs_f)      \
-    X(TYA, tya_f)
+#define OPCODES_LIST          \
+    X(ADC, adc_f, "ADC")      \
+    X(AND, and_f, "AND")      \
+    X(ASL, asl_f, "ASL")      \
+    X(BCC, bcc_f, "BCC")      \
+    X(BCS, bcs_f, "BCS")      \
+    X(BEQ, beq_f, "BEQ")      \
+    X(BIT, bit_f, "BIT")      \
+    X(BMI, bmi_f, "BMI")      \
+    X(BNE, bne_f, "BNE")      \
+    X(BPL, bpl_f, "BPL")      \
+    X(BRK, brk_f, "BRK")      \
+    X(BVC, bvc_f, "BVC")      \
+    X(BVS, bvs_f, "BVS")      \
+    X(CLC, clc_f, "CLC")      \
+    X(CLD, cld_f, "CLD")      \
+    X(CLI, cli_f, "CLI")      \
+    X(CLV, clv_f, "CLV")      \
+    X(CMP, cmp_f, "CMP")      \
+    X(CPX, cpx_f, "CPX")      \
+    X(CPY, cpy_f, "CPY")      \
+    X(DEC, dec_f, "DEC")      \
+    X(DEX, dex_f, "DEX")      \
+    X(DEY, dey_f, "DEY")      \
+    X(FOR, for_f, "FOR")      \
+    X(INC, inc_f, "INC")      \
+    X(INX, inx_f, "INX")      \
+    X(INY, iny_f, "INY")      \
+    X(JMP, jmp_f, "JMP")      \
+    X(JSR, jsr_f, "JSR")      \
+    X(LDA, lda_f, "LDA")      \
+    X(LDX, ldx_f, "LDX")      \
+    X(LDY, ldy_f, "LDY")      \
+    X(LSR, lsr_f, "LSR")      \
+    X(NOP, nop_f, "NOP")      \
+    X(ORA, ora_f, "ORA")      \
+    X(PHA, pha_f, "PHA")      \
+    X(PHP, php_f, "PHP")      \
+    X(PLA, pla_f, "PLA")      \
+    X(PLP, plp_f, "PLP")      \
+    X(ROL, rol_f, "ROL")      \
+    X(ROR, ror_f, "ROR")      \
+    X(RTI, rti_f, "RTI")      \
+    X(RTS, rts_f, "RTS")      \
+    X(SBC, sbc_f, "SBC")      \
+    X(SEC, sec_f, "SEC")      \
+    X(SED, sed_f, "SED")      \
+    X(SEI, sei_f, "SEI")      \
+    X(STA, sta_f, "STA")      \
+    X(STX, stx_f, "STX")      \
+    X(STY, sty_f, "STY")      \
+    X(TAX, tax_f, "TAX")      \
+    X(TAY, tay_f, "TAY")      \
+    X(TSX, tsx_f, "TSX")      \
+    X(TXA, txa_f, "TXA")      \
+    X(TXS, txs_f, "TXS")      \
+    X(TYA, tya_f, "TYA")
 
 typedef enum {
 #define X(opcode, ...) opcode,
