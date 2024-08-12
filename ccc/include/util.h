270c1fc5da60eae0c33724fb244a53e4baa7ae1c
Caleb Stein <caleb@calebstein.net>
2024-08-12 14:01:56 -0700

    add processor flag macros

--- ccc/include/util.h
+++ ccc/include/util.h
@@ -3,4 +3,28 @@
 
 #define MAKE_WORD (((uint16_t)hi << 8) + low)
 
+#define GET_C (p & 1)
+#define GET_Z (p >> 1 & 1)
+#define GET_I (p >> 2 & 1)
+#define GET_D (p >> 3 & 1)
+#define GET_B (p >> 4 & 1)
+#define GET_V (p >> 6 & 1)
+#define GET_N (p >> 7 & 1)
+
+#define SET_C (p |= 1)
+#define SET_Z (p |= 2)
+#define SET_I (p |= 4)
+#define SET_D (p |= 8)
+#define SET_B (p |= 16)
+#define SET_V (p |= 64)
+#define SET_N (p |= 128)
+
+#define UNSET_C (p &= 254)
+#define UNSET_Z (p &= 253)
+#define UNSET_I (p &= 251)
+#define UNSET_D (p &= 247)
+#define UNSET_B (p &= 239)
+#define UNSET_V (p &= 191)
+#define UNSET_N (p &= 127)
+
 #endif //CCC_UTIL_H
