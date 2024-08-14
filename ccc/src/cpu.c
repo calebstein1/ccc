48fc3a7fec1c114e60281dc99344b3c51051b952
Caleb Stein <caleb@calebstein.net>
2024-08-14 13:22:29 -0700

    move sp

--- ccc/src/cpu.c
+++ ccc/src/cpu.c
@@ -7,7 +7,7 @@
 // RAM, Program Counter, and Stack Pointer
 uint8_t prg_ram[0xffff] = {};
 uint8_t *pc = &prg_ram[0x8000];
-uint8_t *sp = prg_ram;
+uint8_t *sp = &prg_ram[0x1ff];
 
 // CPU Registers
 uint8_t a = 0x0, x = 0x0, y = 0x0;
