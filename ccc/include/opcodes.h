dd240bb3d567e31c1d6fb3d7a6f929d5bf7d44ee
Caleb Stein <caleb@calebstein.net>
2024-08-16 10:34:42 -0700

    fill out opcodes

--- ccc/include/opcodes.h
+++ ccc/include/opcodes.h
@@ -9,34 +9,26 @@
     X(ADC_ZX, adczx_f, "ADC") \
     X(ADC_ZY, adczy_f, "ADC") \
     X(ADC_I, adci_f, "ADC")   \
-    X(AND, and_f, "AND")      \
-    X(ASL, asl_f, "ASL")      \
-    X(BCC_A, bcca_f, "BCC")      \
-    X(BCC_AX, bccax_f, "BCC")      \
-    X(BCC_AY, bccay_f, "BCC")      \
-    X(BCC_Z, bccz_f, "BCC")      \
-    X(BCC_ZX, bcczx_f, "BCC")      \
-    X(BCC_ZY, bcczy_f, "BCC")      \
-    X(BCS_A, bcsa_f, "BCS")      \
-    X(BCS_AX, bcsax_f, "BCS")      \
-    X(BCS_AY, bcsay_f, "BCS")      \
-    X(BCS_Z, bcsz_f, "BCS")      \
-    X(BCS_ZX, bcszx_f, "BCS")      \
-    X(BCS_ZY, bcszy_f, "BCS")      \
-    X(BEQ_A, beqa_f, "BEQ")      \
-    X(BEQ_AX, beqax_f, "BEQ")      \
-    X(BEQ_AY, beqay_f, "BEQ")      \
-    X(BEQ_Z, beqz_f, "BEQ")      \
-    X(BEQ_ZX, beqzx_f, "BEQ")      \
-    X(BEQ_ZY, beqzy_f, "BEQ")      \
+    X(AND_A, anda_f, "AND")    \
+    X(AND_AX, andax_f, "AND")   \
+    X(AND_AY, anday_f, "AND")   \
+    X(AND_Z, andz_f, "AND")    \
+    X(AND_ZX, andzx_f, "AND")   \
+    X(AND_ZY, andzy_f, "AND")   \
+    X(AND_I, andi_f, "AND")    \
+    X(ASL_A, asla_f, "ASL")    \
+    X(ASL_AX, aslax_f, "ASL")   \
+    X(ASL_AY, aslay_f, "ASL")   \
+    X(ASL_Z, aslz_f, "ASL")    \
+    X(ASL_ZX, aslzx_f, "ASL")   \
+    X(ASL_ZY, aslzy_f, "ASL")   \
+    X(ASL_AC, aslac_f, "ASL")   \
+    X(BCC, bcc_f, "BCC")   \
+    X(BCS, bcs_f, "BCS")   \
+    X(BEQ, beq_f, "BEQ")   \
     X(BIT, bit_f, "BIT")      \
     X(BMI, bmi_f, "BMI")      \
-    X(BNE_A, bnea_f, "BNE")      \
-    X(BNE_AX, bneax_f, "BNE")      \
-    X(BNE_AY, bneay_f, "BNE")      \
-    X(BNE_Z, bnez_f, "BNE")      \
-    X(BNE_ZX, bnezx_f, "BNE")      \
-    X(BNE_ZY, bnezy_f, "BNE")      \
+    X(BNE, bne_f, "BNE")   \
     X(BPL, bpl_f, "BPL")      \
     X(BRK, brk_f, "BRK")      \
     X(BVC, bvc_f, "BVC")      \
@@ -45,28 +37,28 @@
     X(CLD, cld_f, "CLD")      \
     X(CLI, cli_f, "CLI")      \
     X(CLV, clv_f, "CLV")      \
-    X(CMP_A, cmpa_f, "CMP")      \
-    X(CMP_AX, cmpax_f, "CMP")      \
-    X(CMP_AY, cmpay_f, "CMP")      \
-    X(CMP_Z, cmpz_f, "CMP")      \
-    X(CMP_ZX, cmpzx_f, "CMP")      \
-    X(CMP_ZI, cmpzy_f, "CMP")      \
-    X(CMP_I, cmpi_f, "CMP")      \
-    X(CPX_A, cpxa_f, "CPX")      \
-    X(CPX_AX, cpxax_f, "CPX")      \
-    X(CPX_AY, cpxay_f, "CPX")      \
-    X(CPX_Z, cpxz_f, "CPX")      \
-    X(CPX_ZX, cpxzx_f, "CPX")      \
-    X(CPX_ZY, cpxzy_f, "CPX")      \
-    X(CPX_I, cpxi_f, "CPX")      \
-    X(CPY_A, cpya_f, "CPY")      \
-    X(CPY_AX, cpyax_f, "CPY")      \
-    X(CPY_AY, cpyzy_f, "CPY")      \
-    X(CPY_Z, cpyz_f, "CPY")      \
-    X(CPY_ZX, cpyzx_f, "CPY")      \
-    X(CPY_ZY, cpyzy_f, "CPY")      \
-    X(CPY_I, cpyi_f, "CPY")      \
-    X(DEC_A, deca_f, "DEC")      \
+    X(CMP_A, cmpa_f, "CMP")   \
+    X(CMP_AX, cmpax_f, "CMP") \
+    X(CMP_AY, cmpay_f, "CMP") \
+    X(CMP_Z, cmpz_f, "CMP")   \
+    X(CMP_ZX, cmpzx_f, "CMP") \
+    X(CMP_ZI, cmpzy_f, "CMP") \
+    X(CMP_I, cmpi_f, "CMP")   \
+    X(CPX_A, cpxa_f, "CPX")   \
+    X(CPX_AX, cpxax_f, "CPX") \
+    X(CPX_AY, cpxay_f, "CPX") \
+    X(CPX_Z, cpxz_f, "CPX")   \
+    X(CPX_ZX, cpxzx_f, "CPX") \
+    X(CPX_ZY, cpxzy_f, "CPX") \
+    X(CPX_I, cpxi_f, "CPX")   \
+    X(CPY_A, cpya_f, "CPY")   \
+    X(CPY_AX, cpyax_f, "CPY") \
+    X(CPY_AY, cpyzy_f, "CPY") \
+    X(CPY_Z, cpyz_f, "CPY")   \
+    X(CPY_ZX, cpyzx_f, "CPY") \
+    X(CPY_ZY, cpyzy_f, "CPY") \
+    X(CPY_I, cpyi_f, "CPY")   \
+    X(DEC_A, deca_f, "DEC")   \
     X(DEC_AX, decax_f, "DEC")      \
     X(DEC_AY, decay_f, "DEC")      \
     X(DEC_Z, decz_f, "DEC")      \
@@ -74,7 +66,12 @@
     X(DEC_ZY, deczy_f, "DEC")      \
     X(DEX, dex_f, "DEX")      \
     X(DEY, dey_f, "DEY")      \
-    X(FOR, for_f, "FOR")      \
+    X(EOR_A, eora_f, "EOR")      \
+    X(EOR_AX, eorax_f, "EOR")      \
+    X(EOR_AY, eoray_f, "EOR")      \
+    X(EOR_Z, eorz_f, "EOR")      \
+    X(EOR_ZX, eorzx_f, "EOR")      \
+    X(EOR_ZY, eorzy_f, "EOR")      \
     X(INC_A, inca_f, "INC")      \
     X(INC_AX, incax_f, "INC")      \
     X(INC_AY, incay_f, "INC")      \
@@ -111,18 +108,48 @@
     X(LDY_ZX, ldyzx_f, "LDY")      \
     X(LDY_ZY, ldyzy_f, "LDY")      \
     X(LDY_I, ldyi_f, "LDY")      \
-    X(LSR, lsr_f, "LSR")      \
+    X(LSR_A, lsra_f, "LSR")      \
+    X(LSR_AX, lsrax_f, "LSR")      \
+    X(LSR_AY, lsray_f, "LSR")      \
+    X(LSR_Z, lsrz_f, "LSR")      \
+    X(LSR_ZX, lsrzx_f, "LSR")      \
+    X(LSR_ZY, lsrzy_f, "LSR")      \
+    X(LSR_AC, lsrac_f, "LSR")      \
     X(NOP, nop_f, "NOP")      \
-    X(ORA, ora_f, "ORA")      \
+    X(ORA_A, oraa_f, "ORA")      \
+    X(ORA_AX, oraax_f, "ORA")      \
+    X(ORA_AY, oraay_f, "ORA")      \
+    X(ORA_Z, oraz_f, "ORA")      \
+    X(ORA_ZX, orazx_f, "ORA")      \
+    X(ORA_ZY, orazy_f, "ORA")      \
+    X(ORA_I, orai_f, "ORA")      \
     X(PHA, pha_f, "PHA")      \
     X(PHP, php_f, "PHP")      \
     X(PLA, pla_f, "PLA")      \
     X(PLP, plp_f, "PLP")      \
-    X(ROL, rol_f, "ROL")      \
-    X(ROR, ror_f, "ROR")      \
+    X(ROL_A, rola_f, "ROL")      \
+    X(ROL_AX, rolax_f, "ROL")      \
+    X(ROL_AY, rolay_f, "ROL")      \
+    X(ROL_Z, rolz_f, "ROL")      \
+    X(ROL_ZX, rolzx_f, "ROL")      \
+    X(ROL_ZY, rolzy_f, "ROL")      \
+    X(ROL_AC, rolac_a, "ROL")      \
+    X(ROR_A, rora_f, "ROR")      \
+    X(ROR_AX, rorax_f, "ROR")      \
+    X(ROR_AY, roray_f, "ROR")      \
+    X(ROR_Z, rorz_f, "ROR")      \
+    X(ROR_ZX, rorzx_f, "ROR")      \
+    X(ROR_ZY, rorzy_f, "ROR")      \
+    X(ROR_AC, rorac_f, "ROR")      \
     X(RTI, rti_f, "RTI")      \
     X(RTS, rts_f, "RTS")      \
-    X(SBC, sbc_f, "SBC")      \
+    X(SBC_A, sbca_f, "SBC")      \
+    X(SBC_AX, sbcax_f, "SBC")      \
+    X(SBC_AY, sbcay_f, "SBC")      \
+    X(SBC_Z, sbcz_f, "SBC")      \
+    X(SBC_ZX, sbczx_f, "SBC")      \
+    X(SBC_ZY, sbczy_f, "SBC")      \
+    X(SBC_I, sbci_f, "SBC")      \
     X(SEC, sec_f, "SEC")      \
     X(SED, sed_f, "SED")      \
     X(SEI, sei_f, "SEI")      \
