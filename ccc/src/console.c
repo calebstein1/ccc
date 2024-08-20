085341bca48989efab17a4da4f5eca2f02f31fcf
Caleb Stein <caleb@calebstein.net>
2024-08-20 01:04:18 -0700

    console can print 16 bit numbers

--- ccc/src/console.c
+++ ccc/src/console.c
@@ -8,5 +8,5 @@ void print_buffer() {
 }
 
 void print_number() {
-    printf("%d", prg_ram[0x4020]);
+    printf("%d\n", (uint16_t)(prg_ram[0x4020] + (prg_ram[0x4021] ? prg_ram[0x4021] + 0xff : 0)));
 }
