5803cc43cc6fbfee2cb974afcf4e4f15a6b11c81
Caleb Stein <caleb@calebstein.net>
2024-08-23 01:40:35 -0700

    fix primary and secondary buttons

--- ccc/include/controller.h
+++ ccc/include/controller.h
@@ -3,8 +3,8 @@
 
 #define CONTROLLER_BITFIELD (prg_ram[0x4001])
 
-#define K_PRIMARY SDLK_z
-#define K_SECONDARY SDLK_x
+#define K_PRIMARY SDLK_x
+#define K_SECONDARY SDLK_z
 #define K_LEFT SDLK_LEFT
 #define K_RIGHT SDLK_RIGHT
 #define K_UP SDLK_UP
