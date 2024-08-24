a38f3228d6ea310dd4f2d916f5cbc91576b6fa72
Caleb Stein <caleb@calebstein.net>
2024-08-24 15:33:32 -0700

    use explicit binary for setting/unsetting bitfields

--- ccc/src/controller.c
+++ ccc/src/controller.c
@@ -7,42 +7,42 @@ void get_controller_state(SDL_Event *e) {
     while(SDL_PollEvent(e)) {
         if (e->type == SDL_KEYDOWN) {
             if (e->key.keysym.sym == K_PRIMARY) {
-                CONTROLLER_BITFIELD |= 1;
+                CONTROLLER_BITFIELD |= 0b00000001;
             }
             if (e->key.keysym.sym == K_SECONDARY) {
-                CONTROLLER_BITFIELD |= 2;
+                CONTROLLER_BITFIELD |= 0b00000010;
             }
             if (e->key.keysym.sym == K_LEFT) {
-                CONTROLLER_BITFIELD |= 4;
+                CONTROLLER_BITFIELD |= 0b00000100;
             }
             if (e->key.keysym.sym == K_RIGHT) {
-                CONTROLLER_BITFIELD |= 8;
+                CONTROLLER_BITFIELD |= 0b00001000;
             }
             if (e->key.keysym.sym == K_UP) {
-                CONTROLLER_BITFIELD |= 16;
+                CONTROLLER_BITFIELD |= 0b00010000;
             }
             if (e->key.keysym.sym == K_DOWN) {
-                CONTROLLER_BITFIELD |= 32;
+                CONTROLLER_BITFIELD |= 0b00100000;
             }
         }
         if (e->type == SDL_KEYUP) {
             if (e->key.keysym.sym == K_PRIMARY) {
-                CONTROLLER_BITFIELD &= 254;
+                CONTROLLER_BITFIELD &= 0b11111110;
             }
             if (e->key.keysym.sym == K_SECONDARY) {
-                CONTROLLER_BITFIELD &= 253;
+                CONTROLLER_BITFIELD &= 0b11111101;
             }
             if (e->key.keysym.sym == K_LEFT) {
-                CONTROLLER_BITFIELD &= 251;
+                CONTROLLER_BITFIELD &= 0b11111011;
             }
             if (e->key.keysym.sym == K_RIGHT) {
-                CONTROLLER_BITFIELD &= 247;
+                CONTROLLER_BITFIELD &= 0b11110111;
             }
             if (e->key.keysym.sym == K_UP) {
-                CONTROLLER_BITFIELD &= 239;
+                CONTROLLER_BITFIELD &= 0b11101111;
             }
             if (e->key.keysym.sym == K_DOWN) {
-                CONTROLLER_BITFIELD &= 223;
+                CONTROLLER_BITFIELD &= 0b11011111;
             }
         }
         if (e->type == SDL_QUIT) {
