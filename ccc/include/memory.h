331f4b854a51b449dd67d5a06612f4921bc8737e
Caleb Stein <caleb@calebstein.net>
2024-09-05 14:59:45 -0700

    documentation for memory functions

--- ccc/include/memory.h
+++ ccc/include/memory.h
@@ -5,18 +5,18 @@
 
 extern u8 *pc;
 
-u8 read_mem(u16 addr);
-u16 read_mem16(u16 addr);
-void zmem(u16 addr);
-void write_mem(u16 addr, u8 val);
-void write_mem16(u16 addr, u16 val);
-void write_nmem(u16 addr, const u8 *val, u16 n);
-void inc_mem(u16 addr);
-void dec_mem(u16 addr);
-void add_mem(u16 addr, u8 val);
-void subtr_mem(u16 addr, u8 val);
-void and_mem(u16 addr, u8 val);
-void or_mem(u16 addr, u8 val);
-u8 *get_ptr(u16 addr);
+u8 read_mem(u16 addr); /* Read an 8-bit value from memory */
+u16 read_mem16(u16 addr); /* Read a 16-bit value from memory */
+void zmem(u16 addr); /* Clear a memory address */
+void write_mem(u16 addr, u8 val); /* Write an 8-bit value to memory */
+void write_mem16(u16 addr, u16 val); /* Write a 16-bit value to memory */
+void write_nmem(u16 addr, const u8 *val, u16 n); /* Write an array of bytes to contiguous memory */
+void inc_mem(u16 addr); /* Increment the value stored at an address */
+void dec_mem(u16 addr); /* Decrement the value stored at an address */
+void add_mem(u16 addr, u8 val); /* Add a value to an address */
+void subtr_mem(u16 addr, u8 val); /* Subtract a value from an address */
+void and_mem(u16 addr, u8 val); /* Bitwise and with a value stored in memory */
+void or_mem(u16 addr, u8 val); /* Bitwise or with a value stored in memory */
+u8 *get_ptr(u16 addr); /* Get a pointer to a specific address */
 
 #endif /* CCC_MEMORY_H */
